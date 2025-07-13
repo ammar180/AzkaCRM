3bf1a79752265819455571fe206a270b0f08662d
ammar180 <ammaromran360@gmail.com>
2025-07-13 05:57:45 +0300

    configure EF in infrastructure

---
+++ AdminBE/AdminBE.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using AdminBE.Application.RepositoryInterfaces;
+using AdminBE.Infrastructure.EFContext;
+using AdminBE.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdminBE.Infrastructure;
+
+public static class DependencyInjection
+{
+  public static IServiceCollection AddInfrastructure(
+      this IServiceCollection services,
+      IConfiguration configuration) =>
+      services
+          .AddDatabase(configuration);
+
+  private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+  {
+    string? connectionString = configuration.GetConnectionString("Database");
+
+    services.AddDbContext<ApplicationDbContext>(
+        options => options
+            .UseSqlServer(connectionString));
+
+    services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
+    services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+
+
+    return services;
+  }
+}
